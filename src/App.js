4e11272d0d9a4f61957ad2f011df3c2b3e3b64a6
EricYXChen <eric.chen_@hotmail.com>
2024-01-10 23:35:55 -0500

    added some components to landing page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <html>
+        LANDLORD (斗地主)
+    </html>
+  );
+}
+
+export default App;
---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Landing.css';
+function Landing() {
+    return (
+        <html>
+        <h1>LANDLORD (斗地主)</h1>
+        <body>
+            Welcome to Landlord Game! If you do not know the rules, you can find them <a href="https://www.pagat.com/climbing/doudizhu.html">HERE</a>
+        </body>
+        </html>
+    );
+}
+
+export default Landing;
